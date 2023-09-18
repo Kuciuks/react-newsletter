f8f9d78efc0efd5759716cb9c04068a3d75d4f26
Benas <benas.sarachovas@gmail.com>
2023-09-18 18:59:17 +0100

    working on sorting content for specific sections

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,52 +1,37 @@
-import { matchRoutes } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 import '../Styles/Content.css'
 import Articles from "./Articles"
-import { useEffect, useState } from 'react'
 
 export default function Content({newsList}){
 
-    // create different types of object for each of the section-containers
-    const [sectionCount, setSectionCount] = useState(null)
-    console.log(sectionCount)
+    const [dataList, setDataList] = useState()//store the array of articles in a state, will use for altering the valeu alter on
 
     useEffect(()=>{
-        const randomNumber = Math.floor(Math.random() * 2) //set random number to a state
-        setSectionCount(randomNumber)
-    },[])
+        setDataList(newsList)
+    },[newsList])
+    console.log(dataList)
+
+    const handleDataRequest = (type) => {
 
 
+        switch(type){
+            case 'banner':
+                
+            break;
 
-    const sectionTypeContainer = [ //section types
-        {name: "Articles"},
-        {name: "Banner"},
-        {name: "Headlines"}
-    ]
+            case 'article':
 
-    const generateContent = () => { //generates sections
 
-        for(let i = 0; i < sectionCount; i++){ //loop over each section
+            break;
 
-            const randomNumber2 = Math.floor(Math.random() * sectionCount) //generate a random number for the section
-            
-            switch(randomNumber2){ //switch method for rendering different types of sections
-                case '1':
-                    return(<Articles/>)
-                break;
+            case 'headlines':
 
-                case '2':
-                    return(<Banner/>)
-                break;
-
-                case '3':
-                    return(<Headlines/>)
-                break;
-            }
+            break;
         }
     }
-    
 
-    console.log(newsList)
-    newsList ? generateContent() : console.log("NUll")
+
+
 
 
     return(
@@ -65,6 +50,7 @@ export default function Content({newsList}){
 
             <div className='banner-container'>
                     {/* Fetching banners from the API*/}
+                {handleDataRequest('banner')}
             </div>
 
             <div className='headlines-container'>
@@ -74,9 +60,6 @@ export default function Content({newsList}){
             <div className='headlines-container'>
                     {/* Fetching articles from the API similar to articles-container */}
             </div>
-
-            {/* other variations of newslist item layout/styling in a container */}
-            {/* use generateContent for this part */}
         </div>
     )
 }
