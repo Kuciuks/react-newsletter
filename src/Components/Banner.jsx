3fd61c3c1c2eb141d26e981907df37bcdbcc5314
Benas <benas.sarachovas@gmail.com>
2023-09-22 22:50:18 +0100

    added banner layout and responsivness

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 import '../Styles/Banner.css'
 
 export default function Banner({item}){
-
+    const backgroundImageStyle = {
+        backgroundImage: `url(${item.placeholder_img}?lock=1)`,
+        width: '100%'
+      };
     return(
         <Link to={`/news/${item.id}`} className="banner-link-div">
-            <div className='banner-img'>
-                <picture>
-                    <source srcSet={item.placeholder_img + `?lock=1`}/>
-                    <img className='img' src={item.placeholder_img + `?lock=1`} />
-                </picture>
+            <div className='banner-img' style={backgroundImageStyle}>
             </div>
             <div className="banner-details">
                 <div className="banner-title">
