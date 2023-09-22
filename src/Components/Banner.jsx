bc29da369fc1506f1936be7e5c64aeee0f1e003a
Benas <benas.sarachovas@gmail.com>
2023-09-22 18:10:06 +0100

    working on fixing text overflow, hidden error

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,13 +4,22 @@ import '../Styles/Banner.css'
 export default function Banner({item}){
 
     return(
-        <Link to={`/news/${item.id}`}>
-            <div className='img-div'>
+        <Link to={`/news/${item.id}`} className="banner-link-div">
+            <div className='banner-img'>
                 <picture>
                     <source srcSet={item.placeholder_img + `?lock=1`}/>
                     <img className='img' src={item.placeholder_img + `?lock=1`} />
                 </picture>
             </div>
+            <div className="banner-details">
+                <div className="banner-title">
+                    <h1>{item.title}</h1>
+                </div>
+                <div className="banner-source">
+                    <h3>{item.source}</h3>
+                </div>
+            </div>
+            
         </Link>
     )
 }
