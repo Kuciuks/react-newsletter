319fa19efc6d808f498efb092fbdfb396c6b72a8
Benas <benas.sarachovas@gmail.com>
2023-10-01 22:59:56 +0100

    spent 8 hours staring at this peace of shit and still cant figure out why the articles getting rerendered twice or why the the renderArticles function is getting called double the times even if its called 3 times, fuck reacts reconciliation

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,63 +1,56 @@
-import { useDebugValue, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import '../Styles/Content.css'
 import Articles from "./Articles"
 import Banner from './Banner'
 import { useNewsData } from '../Provider/NewsDataContext';
 
+
+
 export default function Content(){
 
-    const dataArray = useNewsData()
+    const dataList = useNewsData()
     const useData = useRef(null)
-
-
+    let i = 0;
     useEffect(()=>{
-        useData.current = dataArray
-    },[dataArray])
+        useData.current = dataList
+    },[dataList])
+
+    const renderArticles = (type)=>{
+        console.log(i++)
+        let articleList = []
+        const currentData = [...useData.current]
 
 
-    const renderContainers = (type) => {
-            let articleList = []
-
-            switch(type){
-                case 'article':
-                    console.log(type,' call')
-                    articleList = useData.current.slice(0,9)
-                    useData.current.splice(0,9)
-
-                    return(
-                        articleList.map((item,index)=>(
-                            <div className='article-div' key={index}>
-                                <Articles item={item}/>
-                            </div>
-                        ))
-                    )
-
-                case 'banner':
-                    console.log(type,' call')
+        switch(type){
+            case 'article':
+                articleList = currentData.slice(0,9)
+                console.log(articleList,'article list')
+                useData.current = currentData.splice(9)
+                // console.log(useData.current,'usedata inside')
 
                 break;
-
-                default:
-                    console.log('Call name not recognized - ',type, '\n useData length: ',useData.current.length)
-            }
+        }
+        return(
+            articleList.map((item,index)=>(
+                <div className='article-div' key={index}>
+                    <Articles item={item}/>
+                </div>
+            ))
+        )
     }
-
-    
-
-
-    console.log('useData length: ',useData.current)
-
-
-
+    console.log(useData,'usedata outside')
     return(
-        <div className="content-container">
+        <div className='content-container'>
             <div className='articles-container'>
-                {useData.current === null ? <div>Loading...</div> : renderContainers("article")}
+                {useData.current !== null ? renderArticles('article') : <div>Loading...</div>}
             </div>
-
             <div className='articles-container'>
-                {useData.current === null ? <div>Loading...</div> : renderContainers("article")}
+                {useData.current !== null ? renderArticles('article') : <div>Loading...</div>}
+            </div>
+            <div className='articles-container'>
+                {useData.current !== null ? renderArticles('article') : <div>Loading...</div>}
             </div>
         </div>
     )
+
 }
