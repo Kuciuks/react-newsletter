3d67ef6417dd9aeaa212f281a24118a490e0de64
Benas <benas.sarachovas@gmail.com>
2023-10-05 22:38:00 +0100

    adding styling to the components

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -4,8 +4,8 @@ import '../Styles/Featured.css'
 export default function Featured({item}) {
 
     const adjustTitle = (string) => {
-        if(string.length > 35){
-            let newString = string.slice(0,35)
+        if(string.length > 20){
+            let newString = string.slice(0,20)
             return newString
         }else{
             return string
@@ -15,7 +15,11 @@ export default function Featured({item}) {
 
   return (
     <Link to={`/news/${item.id}`} className='featured-link-div'>
-        <div className='featured-title-div'>• {adjustTitle(item.title)}</div>
+        <div className='featured-title-div'>
+            <span>•</span>
+            <span>{adjustTitle(item.title)}</span>
+            <span>•</span>
+        </div>
     </Link>
   )
 }
