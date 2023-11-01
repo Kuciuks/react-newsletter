fc09c7c32872568f960e55da2c1418d94a32e85f
Benas <benas.sarachovas@gmail.com>
2023-11-01 14:02:13 +0000

    added layout for search results

--- src/Components/ResultArticle.jsx
+++ src/Components/ResultArticle.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import '../Styles/ResultArticle.css'
 
 export default function ResultArticle({article}) {
   return (
-    <Link>
-        <div>{article.source}</div>
+    <Link to={`/news/${article.id}`} className='result-link-container'>
+        <div className='result-pic-div'>
+            <picture>
+                <source srcSet={article.placeholder_img}/>
+                <img className='result-img' src={article.placeholder_img}/>
+            </picture>
+        </div>
+        <div className='result-title'>
+            <h2>{article.title}</h2>
+        </div>
+        <div className='result-source'>
+            <p>{article.source}</p>
+        </div>
     </Link>
   )
 }
--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ResultArticle from "./ResultArticle";
+import '../Styles/SearchResults.css'
 
 export default function SearchResults({results}) {
 
